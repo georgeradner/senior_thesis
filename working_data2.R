55fe9fac331b4fba061c638e7375bb18bf2d1c2f
George Radner <georgehradner@gmail.com>
2019-01-07 09:37:30 -0800

    update: pooling + improved matching
    - pooled plant closure data by congressional district 
    - improved county name to fip code matching

--- working_data2.R
+++ working_data2.R
@@ -116,8 +116,14 @@ fip.codes[,4] <- toupper(fip.codes[,4])
 #removing commas and apostrophes from county names
 fip.codes[,5] <- gsub(",","",fip.codes[,4],fixed=TRUE)
 fip.codes[,6] <- gsub("'","", fip.codes[,5],fixed=TRUE)
+fip.codes[,8] <- gsub(" ","",fip.codes[,6],fixed=TRUE)
+fip.codes[,8] <- gsub("-","",fip.codes[,8],fixed=TRUE)
+fip.codes[,8] <- gsub("BOROUGH","",fip.codes[,8],fixed=TRUE)
+fip.codes[,8] <- gsub("PARISH","",fip.codes[,8],fixed=TRUE)
+fip.codes[,8] <- gsub("CENSUSAREA","",fip.codes[,8],fixed=TRUE)
 
 colnames(fip.codes)[6] <- "county.state"
+colnames(fip.codes)[8] <- "county.state1"
 
 #function to extract state name abbreviations when the string has format like "Bibb County, AL".
 #later it will be useful to match state abbreviations to their fip code.
@@ -141,35 +147,54 @@ county_sector1 <- merge(county_sector1, states,all.x = TRUE)
 county_sector1[,11] <- paste(county_sector1$county,county_sector1$state.abb,sep = " ")
 colnames(county_sector1)[11] <- "county.state"
 
+county_sector1[,14] <- gsub(" ","",county_sector1$county.state,fixed=TRUE)
+county_sector1[,14] <- gsub("'","",county_sector1[,14],fixed=TRUE)
+county_sector1[,14] <- gsub("-","",county_sector1[,14],fixed=TRUE)
+county_sector1[,14] <- gsub("BOROUGH","",county_sector1[,14],fixed=TRUE)
+county_sector1[,14] <- gsub("PARISH","",county_sector1[,14],fixed=TRUE)
+county_sector1[,14] <- gsub("CENSUSAREA","",county_sector1[,14],fixed=TRUE)
+county_sector1[,14] <- gsub("STATETOTAL","STATEWIDE",county_sector1[,14],fixed=TRUE)
+
+colnames(county_sector1)[14] <- "county.state1"
+
+
+
 # adding column for difference between estalbishment births and deaths
 county_sector1[,(1+length(colnames(county_sector1)))] <- county_sector1$births - county_sector1$deaths
 colnames(county_sector1)[length(colnames(county_sector1))] <- "net"  
   
 #merge fip codes and county names 
-county_sector3 <- merge(county_sector1,fip.codes[,c(1,2,6)], all.x = TRUE)
+county_sector3 <- merge(county_sector1[,-11],fip.codes[,c(1,2,8)], all.x = TRUE)
+
+#county_sector3 <- merge(county_sector1,fip.codes[,c(1,2,6)], all.x = TRUE)
 
 #unfortunately, ONLY 90% OF COUNTY NAMES IN COUNTY_SECTOR1 MATCHED THE FIP CODES FILE
 
 #the code in comments below shows how I determined 10% was lost
 
-#missing <- c()
-#for (n in 1:length(county_sector3$fipscty)){
-#  if (is.na(county_sector3$fipscty[n])){missing[length(missing)+1] <- n}
-#}
+missing <- c()
+for (n in 1:length(county_sector3$fipscty)){
+  if (is.na(county_sector3$fipscty[n])){missing[length(missing)+1] <- n}
+}
 
-#missing.na <- county_sector3[missing,1]
+missing.na <- county_sector3[missing,1]
 
-#for (n in length(county_sector3$fipscty)){
-#  if (is.na(county_sector3$fipscty[n])){missing[length(missing)+1] <- n}
-#}
+for (n in length(county_sector3$fipscty)){
+  if (is.na(county_sector3$fipscty[n])){missing[length(missing)+1] <- n}
+}
 
-#missing.table <- as.data.frame(table(missing.na))
-#missing.table5 <- missing.table
+missing.table <- as.data.frame(table(missing.na))
+missing.table5 <- missing.table #previous one
+
+
+missing.table8 <- county_sector3[which(is.na(county_sector3$fipscty)),c(1,2,3,14,15)]
 
 
 # we've lost 10% of our data but pushing forward for now. will recover missing 10% later
-county_sector4 <- merge(county_sector1,fip.codes[,c(1,2,6)])
 
+
+county_sector4 <- merge(county_sector1[,-11],fip.codes[,c(1,2,8)])
+colnames(county_sector4)[1] <- "county.state"
 
 # elections occur the congressional district level, so next we'll merge counties and congressional districts
 
@@ -235,15 +260,38 @@ county_sector6 <- merge(county_sector5, cdc,
                         by=c("fipstate","fipscty","period"))
 
 
+
+netDistrict <- function(statedistrict,naics,year){
+  cs6rows <- which(statedistrict==county_sector6$state.district &
+                    naics==county_sector6$naics &
+                    year==county_sector6$initial.year)
+  sum(county_sector6[cs6rows,16]/county_sector6[cs6rows,20])
+  }
+
+cd.closures <- unique(county_sector6[,c(17,8,4)])
+cd.closures <- cbind(cd.closures,data.frame(net = mapply(netDistrict,cd.closures$state.district,cd.closures$naics,cd.closures$initial.year)))
+  
+#sum(county_sector6[which(statedistrict==county_sector6$state.district, naics==county_sector6$naics,year==county_sector6$initial.year),16]*county_sector6[which(statedistrict==county_sector6$state.district, naics==county_sector6$naics, year==county_sector6$initial.year),20])
+
+#cdX = cty1, cyt2, ... ctyN
+#netX = cty1*1/k1 + ... ctyN/kN
 #DRAFT MODEL 1 DATASET: period "2002-2010" + "2012-"
 
 #goal: 
-# CD    YEAR   NET
-# 4,23  2003   -5
+# CD    YEAR  INDUSTRY NET
+# 4,23  2003  23       -5
+
+
+
 
 # LOCAL AREA LABOR FORCE STATISTICS
 
 #importing files with unemployment data by county-year
+lau94 <- read.table("laucnty94.csv",sep=",",header=TRUE,stringsAsFactors = FALSE,skip=4)
+lau95 <- read.table("laucnty95.csv",sep=",",header=TRUE,stringsAsFactors = FALSE,skip=4)
+lau96 <- read.table("laucnty96.csv",sep=",",header=TRUE,stringsAsFactors = FALSE,skip=4)
+lau97 <- read.table("laucnty97.csv",sep=",",header=TRUE,stringsAsFactors = FALSE,skip=4)
+lau98 <- read.table("laucnty98.csv",sep=",",header=TRUE,stringsAsFactors = FALSE,skip=4)
 lau99 <- read.table("laucnty99.csv",sep=",",header=TRUE,stringsAsFactors = FALSE,skip=4)
 lau00 <- read.table("laucnty00.csv",sep=",",header=TRUE,stringsAsFactors = FALSE,skip=4)
 lau01 <- read.table("laucnty01.csv",sep=",",header=TRUE,stringsAsFactors = FALSE,skip=4)
@@ -264,13 +312,44 @@ lau15 <- read.table("laucnty15.csv",sep=",",header=TRUE,stringsAsFactors = FALSE
 lau16 <- read.table("laucnty16.csv",sep=",",header=TRUE,stringsAsFactors = FALSE,skip=4)
 
 lau <- do.call(rbind,
-               list(lau99,lau00,lau01,lau02,lau03,lau04,lau05,lau06,lau07,lau08,lau09,lau10,lau11,lau12,lau13,lau14,lau15,lau16))
+               list(lau94,lau95,lau96,lau97,lau98,lau99,lau00,lau01,lau02,lau03,lau04,lau05,lau06,lau07,lau08,lau09,lau10,lau11,lau12,lau13,lau14,lau15,lau16))
 
 #removing an empty row and an empty column
 lau <- lau[-1,]
 lau <- lau[,-5]
 
 colnames(lau) <- c("cn.code","fipstate","fipscty","initial.year","labor.force","employed","unemployed","urate")
+
+k.l <- length(colnames(lau))
+lau[,(1+k.l)] <- paste(lau$fipstate,lau$fipscty,sep=",")
+colnames(lau)[1+k.l] <- "state.county"
+
+lau[,(2+k.l)] <- as.numeric(lau$urate)
+
+#previousUrate <- function(statecounty,year){
+#  x<-lau[which(statecounty==lau$state.county & lau$initial.year==(year-1)),10]
+#  if(is.na(x[1])){result<-NA
+#  } else {result<-x}
+#  return(x)
+#}
+
+previousUrate <- function(statecounty,year){
+  if(0==length(which(statecounty==lau$state.county
+           & lau$initial.year==(year-1)))) {NA
+  } else {lau[which(statecounty==lau$state.county & lau$initial.year==(year-1)),10]}
+}
+
+lau[,(3+k.l)] <- rep(NA,length(rownames(lau)))
+
+lau[,(3+k.l)] <- mapply(previousUrate,lau$state.county,lau$initial.year)
+
+for(n in 1:length(rownames(lau))){
+  lau[n,(3+k.l)] <- previousUrate(lau$state.county[n],lau$initial.year[n])
+}
+
+lau[,(4+k.l)] <- lau$V10 - lau$V11
+
+
 
 # importing nominate score data
 
@@ -303,6 +382,50 @@ k.dw <- length(colnames(dw_nominate))
 dw_nominate[,(1+k.dw)] <- sapply(dw_nominate$congress.number,congressToElectionYear)
 colnames(dw_nominate)[k.dw+1] <- "election.year"
 
+#create delta.abs.dim1, if.incumbent, and if.flip columns
+
+dw_nominate <- cbind(dw_nominate,data.frame(prev.dim1 = rep(NA,length(rownames(dw_nominate))),
+                                            prev.leg = rep(NA,length(rownames(dw_nominate))),
+                                            prev.party = rep(NA,length(rownames(dw_nominate)))))
+
+previousCongressVal <- function(statedistrict,electionyear,column){
+  if(0==length(which(statedistrict==dw_nominate$state.district
+                     & dw_nominate$election.year==(electionyear-2)))) {NA
+  } else {dw_nominate[(which(statedistrict==dw_nominate$state.district
+                     & dw_nominate$election.year==(electionyear-2))),column]}
+}
+
+dw_nominate$prev.dim1 <- mapply(previousCongressVal,
+                                dw_nominate$state.district,
+                                dw_nominate$election.year,
+                                8) #column 8: dim1
+
+dw_nominate$prev.leg <- mapply(previousCongressVal,
+                               dw_nominate$state.district,
+                               dw_nominate$election.year,
+                               7) #column 7: leg.name
+
+dw_nominate$prev.party <- mapply(previousCongressVal,
+                                 dw_nominate$state.district,
+                                 dw_nominate$election.year,
+                                 6) #column 7: party.code
+
+commonVal <- function(list1,list2){
+  any(list1==list2)
+}
+
+dw_nominate <- cbind(dw_nominate,data.frame(if.incumbent = mapply(commonVal,dw_nominate$leg.name,dw_nominate$prev.leg),
+                                            if.hold = mapply(commonVal,dw_nominate$party.code,dw_nominate$prev.party),
+                                            abs.dim1 = abs(dw_nominate$dim1),
+                                            abs.prev.dim1 = abs(sapply(dw_nominate$prev.dim1,mean))))
+
+dw_nominate <- cbind(dw_nominate, data.frame(dlt.abs.dim1 = dw_nominate$abs.dim1 - dw_nominate$abs.prev.dim1))
+
+
+
+
+#for (n in 1:length(rownames(dw_nominate))){
+#  dw_nominate$prev.dim1[n] <- }
 
 
 #mass layoff dataset
@@ -355,6 +478,74 @@ laid.off3 <- as.numeric(levels(laid.off2))[laid.off2]
 anes_timeseries_cdf[,(2+length.anes)] <- laid.off1
 
 
-#data.range <- cbind(cbind(anes_timeseries_cdf[,c(2,949,950)], laid.off2), laid.off3)
-#data.range <- data.range[!(rowSums(is.na(data.range))),]
+anes.data.range <- cbind(cbind(anes_timeseries_cdf[,c(2,949,950)], laid.off2), laid.off3)
+anes.data.range <- anes.data.range[!(rowSums(is.na(anes.data.range))),]
+anes.data.range <- anes.data.range[,c(1,2,5)]
 
+colnames(anes.data.range) <- c("year","ideo","if.laidoff")
+
+anes.data.range94 <- anes.data.range[which(anes.data.range$year >= 1994),]
+
+key.vars1 <- c("anes.data.range94$ideo",
+               "anes.data.range94$if.laidoff",
+               "dw_nominate$abs.dim1",
+               "mass_layoffs$total",
+               "lau$V10")
+
+
+  
+sum.table1 <- data.frame(mean = rep(NA,length(key.vars1)),
+                         SD = rep(NA,length(key.vars1)),
+                         SE = rep(NA,length(key.vars1)),
+                         min = rep(NA,length(key.vars1)),
+                         max = rep(NA,length(key.vars1)))
+
+rownames(sum.table1) <- c("ideology","recent layoff","absolute nominate score","# mass layoffs","unemployment rate")
+sum.table1[1,] <- c(mean(anes.data.range94$ideo),
+                    sd(anes.data.range94$ideo),
+                    sqrt(var(anes.data.range94$ideo)/length(anes.data.range94$ideo)),
+                    min(anes.data.range94$ideo),
+                    max(anes.data.range94$ideo))
+
+sum.table1[2,] <- c(mean(anes.data.range94$if.laidoff),
+                    sd(anes.data.range94$if.laidoff),
+                    sqrt(var(anes.data.range94$if.laidoff)/length(anes.data.range94$if.laidoff)),
+                    min(anes.data.range94$if.laidoff),
+                    max(anes.data.range94$if.laidoff))
+
+sum.table1[3,] <- c(mean(dw_nominate$abs.dim1),
+                    sd(dw_nominate$abs.dim1),
+                    sqrt(var(dw_nominate$abs.dim1)/length(dw_nominate$abs.dim1)),
+                    min(dw_nominate$abs.dim1),
+                    max(dw_nominate$abs.dim1))
+
+sum.table1[4,] <- c(mean(mass_layoffs$total),
+                    sd(mass_layoffs$total),
+                    sqrt(var(mass_layoffs$total)/length(mass_layoffs$total)),
+                    min(mass_layoffs$total),
+                    max(mass_layoffs$total))
+
+sum.table1[5,] <- c(mean(lau$V10,na.rm = TRUE),
+                    sd(lau$V10,na.rm = TRUE),
+                    sqrt(var(lau$V10,na.rm=TRUE)/length(lau$V10[!is.na(lau$V10)])),
+                    min(lau$V10,na.rm = TRUE),
+                    max(lau$V10,na.rm=TRUE))
+
+
+
+#for (n in 1:length(key.vars1)){
+   #df_n = get(strsplit(key.vars1[n],"$",fixed=TRUE)[1])
+  #var_n = df_n$strsplit(key.vars1[n],"$",fixed=TRUE)[2]
+  #sum.table1[n,] <- c(mean(var_n), sqrt(var(var_n)/length(var_n)),
+                    #min(var_n),
+                    #  max(var_n))
+#}
+  
+#sum.table
+#             mean se min max
+# ideo
+# if.laidoff
+# net
+# urate
+
+write.table(fip.codes, "~/R Stuff/fip.codes.csv",sep=",")
